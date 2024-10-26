14b6b60b8d883db3c74d9424df4bff889640fb81
Jacky <me@jackyu.cn>
2024-10-26 23:02:35 +0800

    enhance(pagination): set default pagesize to 10 to prevent divide by zero panic

--- model/pagination.go
+++ model/pagination.go
@@ -1,22 +1,26 @@
 package model
 
 type Pagination struct {
-    Total       int64 `json:"total"`
-    PerPage     int   `json:"per_page"`
-    CurrentPage int   `json:"current_page"`
-    TotalPages  int64 `json:"total_pages"`
+	Total       int64 `json:"total"`
+	PerPage     int   `json:"per_page"`
+	CurrentPage int   `json:"current_page"`
+	TotalPages  int64 `json:"total_pages"`
 }
 
 type DataList struct {
-    Data       interface{} `json:"data"`
-    Pagination Pagination  `json:"pagination,omitempty"`
+	Data       interface{} `json:"data"`
+	Pagination Pagination  `json:"pagination,omitempty"`
 }
 
 // TotalPage calculate total page
 func TotalPage(total int64, pageSize int) int64 {
-    n := total / int64(pageSize)
-    if total%int64(pageSize) > 0 {
-        n++
-    }
-    return n
+	// fix: divide by zero
+	if pageSize == 0 {
+		pageSize = 10
+	}
+	n := total / int64(pageSize)
+	if total%int64(pageSize) > 0 {
+		n++
+	}
+	return n
 }
